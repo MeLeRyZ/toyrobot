696bc49694d6ef43f0f3927740cee3f1200f9dbb
MeLeRyZ <meleryz@gmail.com>
2018-03-14 12:42:24 +0200

    VERSION: 0.9 + fix mistakes

--- test/toyrobot_test.exs
+++ test/toyrobot_test.exs
@@ -1,97 +1,84 @@
 defmodule ToyRobotTest do
   use ExUnit.Case
-  #import ToyRobot
-
-  test "places the ToyRobot on the table in default position" do
-    assert ToyRobot.place == %ToyRobot.Position{x: 0, y: 0, facing: :north}
-  end
-
-  test "places th ToyRobot on the table in specified position" do
-      assert ToyRobot.place(1, 2, :south) == %ToyRobot.Position{x: 1, y: 2, facing: :south}
-  end
-
-  test "provides th report of the robot's position" do
-      robot = ToyRobot.place(2, 3, :west)
-      assert ToyRobot.report(robot) == {2, 3, :west}
-  end
+  doctest ToyRobot
 
   test "rotates the robot to the right" do
-          {:ok, robot} = ToyRobot.place(0, 0, :north)
-          position = robot |> ToyRobot.right |> ToyRobot.report
-          assert position == {0, 0, :east}
+    {:ok, robot} = ToyRobot.place(0, 0, :north)
+    position = robot |> ToyRobot.right |> ToyRobot.report
+    assert position == {0, 0, :east}
 
-          position = robot |> ToyRobot.right |> ToyRobot.right |> ToyRobot.report
-          assert position == {0, 0, :south}
+    position = robot |> ToyRobot.right |> ToyRobot.right |> ToyRobot.report
+    assert position == {0, 0, :south}
   end
 
   test "rotates the robot to the left" do
-      {:ok, robot} = ToyRobot.place(0, 0, :north)
-      position = robot |> ToyRobot.left |> ToyRobot.report
-      assert position == {0, 0, :west}
-    end
+    {:ok, robot} = ToyRobot.place(0, 0, :north)
+    position = robot |> ToyRobot.left |> ToyRobot.report
 
-    test "rotating the robot 3 times to the right is the same as rotating it to the left" do
-        {:ok, robot} = ToyRobot.place(0, 0, :north)
-        right_position = robot |> ToyRobot.right |> ToyRobot.right |> ToyRobot.right |> ToyRobot.report
-        left_position = robot |> ToyRobot.left |> ToyRobot.report
+    assert position == {0, 0, :west}
+  end
 
-        assert right_position == left_position
-    end
+  test "rotating the robot 3 times to the right is the same as rotate its to the left" do
+    {:ok, robot} = ToyRobot.place(0, 0, :north)
+    right_position = robot |> ToyRobot.right |> ToyRobot.right |> ToyRobot.right |> ToyRobot.report
+    left_position = robot |> ToyRobot.left |> ToyRobot.report
 
-    test "moving robot up if it is facing to the north" do
-        {:ok, robot} = ToyRobot.place(0, 0, :north)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert right_position == left_position
+  end
 
-        assert position == {0, 1, :north}
-    end
+  test "moving robot up if it is facing to the north" do
+    {:ok, robot} = ToyRobot.place(0, 0, :north)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "moving robot right if it is facing to the east" do
-        {:ok, robot} = ToyRobot.place(0, 0, :east)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {0, 1, :north}
+  end
 
-        assert position == {1, 0, :east}
-    end
+  test "moving robot right if it is facing to the east" do
+    {:ok, robot} = ToyRobot.place(0, 0, :east)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "moving robot down if it facing to the south" do
-        {:ok, robot} = ToyRobot.place(4, 4, :south)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {1, 0, :east}
+  end
 
-        assert position == {4, 3, :south
-    end
+  test "moving robot down if it is facing to the south" do
+    {:ok, robot} = ToyRobot.place(4, 4, :south)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "moving robot left if it is facing to the west" do
-        {:ok, robot} = ToyRobot.place(4, 4, :west)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {4, 3, :south}
+  end
 
-        assert position == {3, 4, :west}
-    end
+  test "moving robot left if it is facing to the west" do
+    {:ok, robot} = ToyRobot.place(4, 4, :west)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "prevent the robot to fall going north" do
-        {:ok, robot} = ToyRobot.place(4, 4, :north)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {3, 4, :west}
+  end
 
-        assert position == {4, 4, :north}
-    end
+  test "prevent the robot to fall going north" do
+    {:ok, robot} = ToyRobot.place(4, 4, :north)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "prevent the robot to fall going east" do
-      {:ok, robot} = ToyRobot.place(4, 4, :east)
-      position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {4, 4, :north}
+  end
 
-      assert position == {4, 4, :east}
-    end
+  test "prevent the robot to fall going east" do
+    {:ok, robot} = ToyRobot.place(4, 4, :east)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "prevent the robot to fall going south" do
-        {:ok, robot} = ToyRobot.place(0, 0, :south)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {4, 4, :east}
+  end
 
-        assert position == {0, 0, :south}
-    end
+  test "prevent the robot to fall going south" do
+    {:ok, robot} = ToyRobot.place(0, 0, :south)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
-    test "prevent the robot to fall going west" do
-        {:ok, robot} = ToyRobot.place(0, 0, :west)
-        position = robot |> ToyRobot.move |> ToyRobot.report
+    assert position == {0, 0, :south}
+  end
 
-        assert position == {0, 0, :west}
-    end
+  test "prevent the robot to fall going west" do
+    {:ok, robot} = ToyRobot.place(0, 0, :west)
+    position = robot |> ToyRobot.move |> ToyRobot.report
 
+    assert position == {0, 0, :west}
+  end
 end
