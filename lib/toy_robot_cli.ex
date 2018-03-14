edf2b842177b8deaf5771dbd8512870488324c0d
MeLeRyZ <meleryz@gmail.com>
2018-03-14 13:31:01 +0200

    now we can place our robot where we want

--- lib/toy_robot_cli.ex
+++ lib/toy_robot_cli.ex
@@ -6,17 +6,40 @@ defmodule ToyRobot.CLI do
     end
 
     @commands %{
-        "quit" => "Quits the simulator"
+        "quit" => "Quits the simulator",
+        "place" => "format:'place [X,Y,F]'. " <>
+             "Places the Robot into X,Y facing F (Default is 0,0,North). " <>
+             "Where facing is: north, west, south or east."
+
     }
     defp receive_command do
         IO.gets("\n> ")
         |> String.trim
         |> String.downcase
+        |> String.split(" ")
         |> execute_command
     end
 
-    defp execute_command("quit") do
+    defp execute_command(["quit"]) do
         IO.puts "\nConnection lost..."
+    end
+
+    defp execute_command(["place" | params]) do
+        {x, y, facing} = process_place_params(params)
+        case ToyRobot.place(x, y, facing) do
+            {:ok, _robot} ->
+                receive_command()
+            {:failure, message} ->
+                IO.puts message
+                receive_command()
+        end
+    end
+    defp process_place_params(params) do
+        [x, y, facing] = params
+                            |> Enum.join("")
+                            |> String.split(",")
+                            |> Enum.map(&String.trim/1)
+        {String.to_integer(x), String.to_integer(y), String.to_atom(facing)}
     end
 
     defp execute_command(_unknown) do
