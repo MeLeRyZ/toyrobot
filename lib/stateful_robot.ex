166928adb5f004bf961d29affa1eb1fdd9afe4ad
MeLeRyZ <meleryz@gmail.com>
2018-03-23 13:21:47 +0200

    Change to use /Agent/

--- lib/stateful_robot.ex
+++ lib/stateful_robot.ex
@@ -1,32 +1,36 @@
 defmodule ToyRobot.StatefulRobot do
-    alias ToyRobot.StatefulRobot
+    # alias ToyRobot.StatefulRobot -> Beacuse of Agent
+
     def place do
         {:ok, state} = ToyRobot.place
-        pid = spawn_link(fn -> listen(state) end)
-        Process.register(pid, StatefulRobot)
+        {:ok, pid} = Agent.start(fn -> state end, name: __MODULE__)
         pid
     end
 
-    def listen(state) do
-        receive do
-            {:report, pid} ->
-                send(pid, state)
-                listen(state)
-            {:move} -> ToyRobot.move(state) |> listen
-            {:left} -> ToyRobot.left(state) |> listen
-            {:right} -> ToyRobot.right(state) |> listen
-        end
-    end
+    #  NO NEED WITH Agent
+    # def listen(state) do
+    #     receive do
+    #         {:report, pid} ->
+    #             send(pid, state)
+    #             listen(state)
+    #         {:move} -> ToyRobot.move(state) |> listen
+    #         {:left} -> ToyRobot.left(state) |> listen
+    #         {:right} -> ToyRobot.right(state) |> listen
+    #     end
+    # end
 
-    def move, do: send(StatefulRobot, {:move})
-    def left, do: send(StatefulRobot, {:left})
-    def right, do: send(StatefulRobot, {:right})
+    def move, do: Agent.update(__MODULE__, &(ToyRobot.move(&1)))
+    def left, do: Agent.update(__MODULE__, &(ToyRobot.left(&1)))
+    def right, do: Agent.update(__MODULE__, &(ToyRobot.right(&1)))
 
     def report do
-        send(StatefulRobot, {:report, self()})
-
-        receive do
-            state -> ToyRobot.report(state) 
-        end
+        Agent.get(__MODULE__, &(ToyRobot.report(&1))) #EQUALS " fn state -> ToyRobot.report(state) end "
     end
+    # def report do
+    #     send(StatefulRobot, {:report, self()})
+    #
+    #     receive do
+    #         state -> ToyRobot.report(state)
+    #     end
+    # end
 end
